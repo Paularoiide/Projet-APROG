b749cacbac8fbd6e2c93c08749938aa285691110
Charles de Bourgoing <charles.debourgoing@gmail.com>
2025-04-11 14:00:56 +0200

    inclusion des fichier vec tor et physics.h

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
+// Slime, jeu de l'atelier APROG, 2025
+
 #include <iostream>
 #include <cassert>
 using namespace std;
 #include <Imagine/Graphics.h>
 using namespace Imagine;
 
+#include "physics.h"
+#include "vector.h"
+
 struct Slime{
     Color color;
     int rayon = 10;
+    void lancer(Vector cible);
+
 };
 
 int main()
