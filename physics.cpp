45207457943477c2a6340f944ba4a1f07a794370
Charles de Bourgoing <charles.debourgoing@gmail.com>
2025-05-20 11:11:02 +0200

    friction simple ajout

--- physics.cpp
+++ physics.cpp
@@ -1,12 +1,16 @@
 #include "physics.h"
 
 
-Vector Acceleration(Vector v, double friction_coeff){
+Vector Acceleration(Vector v, double friction_coeff, double friction_coeff2){
     Vector acc = {0.,0.};
 
     // ajout de la friction
     Vector friction = -friction_coeff * v;
     acc =acc + friction;
+
+    // ajout de la friction constante
+    Vector friction2 = -friction_coeff2*v/sqrt(norm2(v));
+    acc = acc + friction2;
 
     return acc;
 }
